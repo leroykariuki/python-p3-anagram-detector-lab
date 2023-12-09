d0013688103eff3b9dbf43c69dfc1ec1a0d7aca9
leroy <leroy.kariuki@student.moringaschool.com>
2023-12-09 12:15:06 +0300

    anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # Convert to lowercase for case-insensitive comparison
+
+    def match(self, anagram_list):
+        return [anagram for anagram in anagram_list if self.is_anagram(anagram)]
+
+    def is_anagram(self, candidate):
+        # Check if the candidate word is not the same as the initialized word and has the same letters
+        return candidate.lower() != self.word and sorted(candidate.lower()) == sorted(self.word)
